5092a2cba57364f4f5aaf5633e2a0337e4f3b8da
Jordan Bratt <jfbratt@gmail.com>
2016-06-27 15:03:20 -0400

    update the nnv filter and add joining table and joins

--- scripts/county_joining.R
+++ scripts/county_joining.R
@@ -5,8 +5,10 @@ library(stringr)
 library(dplyr)
 
 nnv <- read_tsv("data-raw/nnv-tsv/all-votes.tsv")
-shpfile <- us_counties("1827-01-01", states = "New York")
-all_ny <- USAboundaries::hist_us_counties@data %>% filter(state_terr == "New York") %>% distinct(name)
+all_ny <- USAboundaries::hist_us_counties@data %>% filter(state_terr == "New York")
+troublesome_elections <- read_csv("data/troublesome_elections.csv")
+county_join_table <- read_csv("data/county_join_table.csv") %>%
+  select(nnv_county, ahcb_county)
 
 # Clean up variable names, filter and create distinct counties data table
 names(nnv) <- names(nnv) %>%
@@ -15,27 +17,17 @@ names(nnv) <- names(nnv) %>%
   str_replace_all("\\s", "_")
 
 ny_ce_county <- nnv %>%
-  filter(office == "U.S. House of Representatives" & state == "New York" & !is.na(county))
-
-ny_county_count <- ny_ce_county %>%
-  distinct(county) %>%
-  select(county, date)
-
-# shpfile: Creating the shapefile's data table, fixing the string case, joining to distinct nnv counties
-shp <-shpfile@data
-
-lcase_name <- str_to_title(shp$name)
-shp <- shp %>%
-  mutate(lower_name = lcase_name)
-
-shp_county_join <- ny_county_count %>%
-  left_join(shp, by = c("county" = "lower_name"))
+  filter(office == "U.S. House of Representatives" & state == "New York" & !is.na(county)) %>%
+  filter(!id %in% troublesome_elections$election_id) %>%
+  count(county)
 
 # all_ny: Creating the shapefile's data table, fixing the string case, joining to distinct nnv counties
-lcase_name <- str_to_title(all_ny$name)
+tcase_name <- str_to_title(all_ny$name)
 all_ny <- all_ny %>%
-  mutate(lower_name = lcase_name)
+  mutate(title_case = tcase_name)
 
-all_ny_county_join <- ny_county_count %>%
-  left_join(all_ny, by = c("county" = "lower_name"))
+ce_join <- ny_ce_county %>%
+  left_join(county_join_table, by = c("county" = "nnv_county"))
 
+ahcb_nnv_join <- ce_join %>%
+  left_join(all_ny, by = c("ahcb_county" = "name"))
