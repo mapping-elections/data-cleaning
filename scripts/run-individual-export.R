0aa634b95ff801557909bc528f8b1cef52c62206
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-08-17 14:14:44 -0400

    Script to run all export checks

--- scripts/run-individual-export.R
+++ scripts/run-individual-export.R
@@ -1,22 +1,43 @@
-# Temporary script to call other script on
+# Temporary script to call other scripts for exporting individual data,
+# then checking it
 library(readr)
 library(dplyr)
 library(stringr)
 library(purrr)
 
+# Export individual elections
 elections <- read_csv("data/congressional-elections-list.csv")
 
-files <- elections %>%
+keepers <- elections %>%
   filter(!special,
          state == "NY",
          str_detect(id, "uscongress"),
-         !str_detect(id, "spring")) %>%
-  `$`("filename")
+         !str_detect(id, "spring"))
+
+files <- keepers$filename
 
 run_export <- function(f) {
   cmd <- str_c("./scripts/xml2table.R data-raw/nnv-xml/", f,
                " -o data/congressional-individual/NY")
+  message(cmd)
   system(cmd)
 }
 
-walk(files[1], run_export)
+walk(files, run_export)
+
+# Create the files that do the checking
+run_check <- function(df) {
+  cmd <- str_c("./scripts/check-election-aggregates.R",
+               "--input", "data/congressional-individual/NY",
+               "--year", df$year,
+               "--state", df$state,
+               "--output", "data/congressional-county-checks/NY",
+               sep = " ")
+  message(cmd)
+  system(cmd)
+}
+
+keepers %>%
+  distinct(state, year) %>%
+  by_row(run_check)
+
