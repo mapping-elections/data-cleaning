191617fd7ea0ebcf73f4d499fe5a46d5605ca242
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-02-03 19:51:25 -0500

    Export AL

--- scripts/export-data-to-repository.R
+++ scripts/export-data-to-repository.R
@@ -6,7 +6,7 @@
 
 # STATES to export
 states <- c("NY", "VA", "NC", "MA", "ME", "NH", "VT", "CT", "RI", "PA", "NJ",
-            "DE", "MD", "SC", "GA")
+            "DE", "MD", "SC", "GA", "AL")
 
 suppressMessages(library(tidyverse))
 suppressMessages(library(stringr))
