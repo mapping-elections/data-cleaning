9ec34defb177b2129117c70ae2661fde4d4c918e
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-12-12 11:34:24 -0500

    Get the columns in the right order

--- scripts/fix-counties-after-affiliation.R
+++ scripts/fix-counties-after-affiliation.R
@@ -9,7 +9,7 @@ suppressPackageStartupMessages(library(mappingelections))
 Usage: fix-counties-after-affiliation.R --map <map>" -> doc
 
 opt <- docopt(doc)
-# opt <- list(map = "meae.congressional.congress01.de.county")
+# opt <- list(map = "meae.congressional.congress01.ny.county")
 
 votes_tidy <- meae_maps %>%
   filter(meae_id == opt$map) %>%
@@ -42,8 +42,40 @@ percentages <- votes_tidy %>%
 total_votes <- votes_tidy %>%
   distinct(county_ahcb, total_vote)
 
+has_column <- function(df, column) {
+  column %in% colnames(df)
+}
+
+add_column <- function(df, column) {
+  if (!has_column(df, column)) {
+    df[, column] <- NA_real_
+  }
+  df
+}
+
 out <- votes %>%
   left_join(percentages, by = "county_ahcb") %>%
-  left_join(total_votes, by = "county_ahcb")
+  left_join(total_votes, by = "county_ahcb") %>%
+  add_column("Anti-Federalist_vote") %>%
+  add_column("Federalist_vote") %>%
+  add_column("Other_vote") %>%
+  add_column("Anti-Federalist_percentage") %>%
+  add_column("Federalist_percentage") %>%
+  add_column("Other_percentage") %>%
+  add_column("Chesapeake_vote") %>%
+  add_column("Chesapeake_percentage") %>%
+  add_column("Potomac_vote") %>%
+  add_column("Potomac_percentage") %>%
+  add_column("Democratic-Republican_vote") %>%
+  add_column("Democratic-Republican_percentage") %>%
+  select(-total_vote) %>%
+  select(county_ahcb,
+         `Federalist_vote`, `Federalist_percentage`,
+         `Anti-Federalist_vote`, `Anti-Federalist_percentage`,
+         `Democratic-Republican_vote`, `Democratic-Republican_percentage`,
+         `Chesapeake_vote`, `Chesapeake_percentage`,
+         `Potomac_vote`, `Potomac_percentage`,
+         `Other_vote`, `Other_percentage`,
+         everything())
 
 write_csv(out, "~/Desktop/fixes-to-parties.csv")
